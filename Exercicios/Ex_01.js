125baee08730a32805c0fe82125818ad069148db
Souzadfs <souzadf09@gmail.com>
2025-11-13 05:21:47 -0300

    Learning about data

---
+++ Exercicios/Ex_01.js
@@ -0,0 +1,7 @@
+{
+    console.log('1')
+    console.log('3')
+    console.log('5')
+    console.log('7')
+    
+}
---
+++ Fundamento Js/dados.js
@@ -0,0 +1,18 @@
+/*
+Nesse capitúlo apredemos a utilizar as variavéis
+
+*/
+{
+var nome = "Caneta";
+var preco = 6.5;
+var quantidade = 10;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log (nome);
+console.log (preco);
+console.log (quantidade);
+console.log (imposto);
+console.log (precoFinal)
+
+}
