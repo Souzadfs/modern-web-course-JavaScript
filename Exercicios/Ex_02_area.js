c5265813133117682dfd147bec3a43eccf569c79
Souzadfs <souzadf09@gmail.com>
2025-11-13 06:07:48 -0300

    Ex 02

---
+++ Exercicios/Ex_02_area.js
@@ -0,0 +1,6 @@
+let raio = 12
+let Pi = 3.14
+
+let CalculoCircunferencia = Pi * raio * raio
+
+console.log(CalculoCircunferencia)
---
+++ Iniciando na Programação Js/dados_03.js
@@ -0,0 +1,6 @@
+let estaChovendo = false
+
+console.log(typeof preco);
+console.log(typeof desconto);
+console.log(typeof nome);
+console.log(typeof estaChovendo);
