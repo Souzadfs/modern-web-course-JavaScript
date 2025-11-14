1e639264264568e546b8c55b5d84c482d774270c
Souzadfs <souzadf09@gmail.com>
2025-11-14 05:10:41 -0300

    aprendendo sobre variaveis

---
+++ Iniciando na Programação Js/variaveisEconstantes.js
@@ -0,0 +1,13 @@
+/*
+Aula 38 explica sobre como usar as variavéis ( VAR, LET e costantes) 
+Const = são variavél que não muda
+Var e Let existem uma difrença entre si
+porém é recomendado usar Let
+*/
+var a = 3
+let b = 10
+
+var a = 20
+ b = 23
+
+console.log(a, b)
